1006fd72e15906b3f475802adbfb3f9d3ed2c76e
vlotysh <lotysh.vm@gmail.com>
2020-10-21 00:52:34 +0300

    Fixed auto hide ios keyboard

--- lib/app/messenger.dart
+++ lib/app/messenger.dart
@@ -1,30 +1,48 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:messenger/app/screens/auth_screen.dart';
 import 'package:messenger/app/screens/home_screen.dart';
 
 class Messenger extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Messenger',
-        theme: ThemeData(
-            primarySwatch: Colors.pink,
-            accentColor: Colors.deepPurple,
-            backgroundColor: Colors.pink,
-            accentColorBrightness: Brightness.dark,
-            buttonTheme: ButtonTheme.of(context).copyWith(
-                buttonColor: Colors.pink,
-                textTheme: ButtonTextTheme.primary,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)))),
-        home: StreamBuilder(
-            stream: FirebaseAuth.instance.onAuthStateChanged,
-            builder: (cxt, userSnapshot) {
-              if (userSnapshot.hasData) {
-                return HomeScreen();
-              }
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: () {
+          if (Platform.isIOS) hideKeyboard(context);
+        },
+        child: MaterialApp(
+          title: 'Messenger',
+          theme: ThemeData(
+              primarySwatch: Colors.pink,
+              accentColor: Colors.deepPurple,
+              backgroundColor: Colors.pink,
+              accentColorBrightness: Brightness.dark,
+              buttonTheme: ButtonTheme.of(context).copyWith(
+                  buttonColor: Colors.pink,
+                  textTheme: ButtonTextTheme.primary,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)))),
+          home: StreamBuilder(
+              stream: FirebaseAuth.instance.onAuthStateChanged,
+              builder: (cxt, userSnapshot) {
+                if (userSnapshot.hasData) {
+                  return HomeScreen();
+                }
 
-              return AuthScreen();
-            }),
+                return AuthScreen();
+              }),
+        ),
       );
+
+  void hideKeyboard(context) {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+
+    FocusManager.instance.primaryFocus.unfocus();
+
+    if (!currentFocus.hasPrimaryFocus) {
+      //SystemChannels.textInput.invokeMethod('TextInput.hide');
+    }
+  }
 }
