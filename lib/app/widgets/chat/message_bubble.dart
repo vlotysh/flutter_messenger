e336a2bd398a5cdfa12e3c92d636430993e94387
vlad lotysh <gangsta1@i.ua>
2020-10-29 17:27:13 +0200

    Updated selection mode inb chat screen

--- lib/app/widgets/chat/message_bubble.dart
+++ lib/app/widgets/chat/message_bubble.dart
@@ -17,6 +17,7 @@ class MessageBubble extends StatelessWidget {
     SelectProvider selectProvider = Provider.of<SelectProvider>(context);
     List<Message> selectedItems = selectProvider.items;
     Message message = Provider.of<Message>(context);
+    bool isSelected = selectProvider.isSelected(message);
 
     return GestureDetector(
       onLongPress: () {
@@ -24,68 +25,69 @@ class MessageBubble extends StatelessWidget {
           toggleSelected(context, message);
         }
       },
+      onTap: () {
+        if (selectedItems.length > 0) {
+          selectProvider.toggle(message);
+        }
+      },
       child: Stack(overflow: Overflow.visible, children: [
-        Row(
-          mainAxisAlignment:
-              message.isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-          children: [
-            if (selectedItems.length > 0) Checkbox(
-              value: selectProvider.isSelected(message),
-              onChanged: (_) {
-                selectProvider.toggle(message);
-                //message.toggleSelect();
-              },
-            ),
-            Container(
-                decoration: BoxDecoration(
-                    color: message.isMe
-                        ? Colors.grey[300]
-                        : Theme.of(context).accentColor,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12),
-                      bottomLeft: !message.isMe
-                          ? Radius.circular(0)
-                          : Radius.circular(12),
-                      bottomRight: message.isMe
-                          ? Radius.circular(0)
-                          : Radius.circular(12),
-                    )),
-                width: 140,
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                margin: EdgeInsets.symmetric(vertical: 15, horizontal: 8),
-                child: Column(
-                  crossAxisAlignment: message.isMe
-                      ? CrossAxisAlignment.end
-                      : CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: message.isMe
-                          ? MainAxisAlignment.end
-                          : MainAxisAlignment.start,
-                      children: [
-                        Text(message.username,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: message.isMe
-                                    ? Colors.black
-                                    : Theme.of(context)
-                                        .accentTextTheme
-                                        .headline1
-                                        .color)),
-                      ],
-                    ),
-                    Text(message.text,
-                        style: TextStyle(
-                            color: message.isMe
-                                ? Colors.black
-                                : Theme.of(context)
-                                    .accentTextTheme
-                                    .headline1
-                                    .color)),
-                  ],
-                )),
-          ],
+        Container(
+          color: isSelected ? Colors.tealAccent[200] : null,
+          child: Row(
+            mainAxisAlignment:
+                message.isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                      color: message.isMe
+                          ? Colors.grey[300]
+                          : Theme.of(context).accentColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                        bottomLeft: !message.isMe
+                            ? Radius.circular(0)
+                            : Radius.circular(12),
+                        bottomRight: message.isMe
+                            ? Radius.circular(0)
+                            : Radius.circular(12),
+                      )),
+                  width: 140,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 8),
+                  child: Column(
+                    crossAxisAlignment: message.isMe
+                        ? CrossAxisAlignment.end
+                        : CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: message.isMe
+                            ? MainAxisAlignment.end
+                            : MainAxisAlignment.start,
+                        children: [
+                          Text(message.username,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: message.isMe
+                                      ? Colors.black
+                                      : Theme.of(context)
+                                          .accentTextTheme
+                                          .headline1
+                                          .color)),
+                        ],
+                      ),
+                      Text(message.text,
+                          style: TextStyle(
+                              color: message.isMe
+                                  ? Colors.black
+                                  : Theme.of(context)
+                                      .accentTextTheme
+                                      .headline1
+                                      .color)),
+                    ],
+                  )),
+            ],
+          ),
         ),
         Positioned(
             top: 0,
