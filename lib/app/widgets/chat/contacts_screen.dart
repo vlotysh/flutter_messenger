ad7401cbdd5dbbec3d0cdad19929e15768198dc5
vlotysh <lotysh.vm@gmail.com>
2020-10-31 18:46:32 +0200

    Added contacts

--- lib/app/widgets/chat/contacts_screen.dart
+++ lib/app/widgets/chat/contacts_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:messenger/app/interfaces/appBarActions.dart';
 
@@ -9,6 +10,26 @@ class ContactsScreen extends StatefulWidget implements AppBarActions {
 class _ContactsScreenState extends State<ContactsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('contacts'));
+    return new FutureBuilder(
+        future: Firestore.instance
+            .collection('users')
+            .orderBy('username')
+            .getDocuments(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return new Text("There is no expense");
+          return ListView(children: getExpenseItems(snapshot));
+        });
+  }
+
+  getExpenseItems(AsyncSnapshot<QuerySnapshot> snapshot) {
+    return snapshot.data.documents
+        .map((doc) => new ListTile(
+            leading: CircleAvatar(
+              radius: 16,
+              backgroundImage: NetworkImage(doc["avatarUrl"]),
+            ),
+            title: new Text(doc["username"]),
+            subtitle: new Text(doc["email"])))
+        .toList();
   }
 }
