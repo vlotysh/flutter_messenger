38c3fd24ddade04885a4977e8d090206136aa0fb
vlotysh <lotysh.vm@gmail.com>
2020-10-31 18:37:05 +0200

    Updated chat view

--- lib/app/widgets/chat/message_bubble.dart
+++ lib/app/widgets/chat/message_bubble.dart
@@ -31,72 +31,79 @@ class MessageBubble extends StatelessWidget {
         }
       },
       child: Stack(overflow: Overflow.visible, children: [
-        Container(
-          color: isSelected ? Colors.tealAccent[200] : null,
-          child: Row(
-            mainAxisAlignment:
-                message.isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                      color: message.isMe
-                          ? Colors.grey[300]
-                          : Theme.of(context).accentColor,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                        bottomLeft: !message.isMe
-                            ? Radius.circular(0)
-                            : Radius.circular(12),
-                        bottomRight: message.isMe
-                            ? Radius.circular(0)
-                            : Radius.circular(12),
-                      )),
-                  width: 140,
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                  margin: EdgeInsets.symmetric(vertical: 15, horizontal: 8),
-                  child: Column(
-                    crossAxisAlignment: message.isMe
-                        ? CrossAxisAlignment.end
-                        : CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: message.isMe
-                            ? MainAxisAlignment.end
-                            : MainAxisAlignment.start,
-                        children: [
-                          Text(message.username,
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: message.isMe
-                                      ? Colors.black
-                                      : Theme.of(context)
-                                          .accentTextTheme
-                                          .headline1
-                                          .color)),
-                        ],
-                      ),
-                      Text(message.text,
-                          style: TextStyle(
-                              color: message.isMe
-                                  ? Colors.black
-                                  : Theme.of(context)
-                                      .accentTextTheme
-                                      .headline1
-                                      .color)),
-                    ],
-                  )),
-            ],
-          ),
+        Column(
+          children: [
+            Container(
+              color: isSelected ? Colors.tealAccent[200] : null,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: message.isMe
+                                ? Colors.grey[300]
+                                : Theme.of(context).accentColor,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(12),
+                              topRight: Radius.circular(12),
+                              bottomLeft: !message.isMe
+                                  ? Radius.circular(0)
+                                  : Radius.circular(12),
+                              bottomRight: message.isMe
+                                  ? Radius.circular(0)
+                                  : Radius.circular(12),
+                            )),
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                        margin: message.isMe
+                            ? EdgeInsets.only(left: 100, top: 15, bottom: 8)
+                            : EdgeInsets.only(
+                                right: 100, left: 35, top: 15, bottom: 8),
+                        child: Column(
+                          crossAxisAlignment: message.isMe
+                              ? CrossAxisAlignment.end
+                              : CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: message.isMe
+                                  ? MainAxisAlignment.end
+                                  : MainAxisAlignment.start,
+                              children: [
+                                Text(message.username,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: message.isMe
+                                            ? Colors.black
+                                            : Theme.of(context)
+                                                .accentTextTheme
+                                                .headline1
+                                                .color)),
+                              ],
+                            ),
+                            Text(message.text,
+                                style: TextStyle(
+                                    color: message.isMe
+                                        ? Colors.black
+                                        : Theme.of(context)
+                                            .accentTextTheme
+                                            .headline1
+                                            .color)),
+                          ],
+                        )),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        Positioned(
-            top: 0,
-            left: message.isMe ? null : 130,
-            right: message.isMe ? 130 : null,
-            child: CircleAvatar(
-              radius: 16,
-              backgroundImage: NetworkImage(message.avatarUrl),
-            )),
+        if (!message.isMe)
+          Positioned(
+              left: 0,
+              bottom: 10,
+              child: CircleAvatar(
+                radius: 16,
+                backgroundImage: NetworkImage(message.avatarUrl),
+              )),
       ]),
     );
   }
